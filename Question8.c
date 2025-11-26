cd53027cdd8861d42ab12734479de6486b02eca0
khyatikaa-svg <khyati2421@gmail.com>
2025-11-26 23:30:31 +0530

    Add files via upload

---
+++ Question8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n1 = 5;
+    int n2 = 10;
+
+    printf("For Input 1:\n");
+    printf("Sum = %d\n", n1 * (n1 + 1) / 2);
+
+    printf("For Input 2:\n");
+    printf("Sum = %d\n", n2 * (n2 + 1) / 2);
+
+    return 0;
+}
