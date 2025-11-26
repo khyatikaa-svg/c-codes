867f00f990ee1622e9f5db934ad87b0c73cecf80
khyatikaa-svg <khyati2421@gmail.com>
2025-11-26 22:19:28 +0530

    Add files via upload

---
+++ Question2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a=10,b=2,c=7,d=3;
+
+    printf("For Input 1:\n");
+    printf("sum = %d ",a + b); 
+    printf("Diff = %d ",a + b);
+    printf("Product = %d ",a + b);
+    printf("Quotient = %d\n", a / b);
+
+    printf()"For Input 2:\n);
+    printf("sum = %d ", c + d);
+    printf("Diff = %d ", c - d);
+    printf("Product = %d ", c * d);
+    printf("Quotient = %d\n", c / d);
+
+    return 0;
+  
+}
