57f58621ec08feeb0afb1a6cf5c176c96a03dd8d
khyatikaa-svg <khyati2421@gmail.com>
2025-11-26 23:28:42 +0530

    Add files via upload

---
+++ Question4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define PI 3.14159
+int main()
+{
+    int radius1 = 7;
+    int radius2 = 3;
+
+    printf("For input 1:\n");
+    printf("Area = %.2f ", PI * radius1 * radius1);
+    printf("Circumference = %.2f\n", 2 * PI * );
+
+    printf(" For Input 2:\n");
+    printf("Area = %.2f ", PI * radius2 * radius2);
+    printf("Circumference = %.2f\n", 2 * PI * radius2);
+
+    return 0;
+}
